7857527143e5c043e9e9b5a0616b548593b4abb2
Anshuman <anshumanshukla.12as@gmail.com>
2025-08-16 12:13:27 +0530

    Doubly Linked List

--- DoublyLinkedList/DoublyLL.java
+++ DoublyLinkedList/DoublyLL.java
@@ -1,5 +1,42 @@
 package DoublyLinkedList;
 
 public class DoublyLL {
+    private Node head;
+    private class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data){
+            this.data = data;
+        }
+        public Node(int data,Node next , Node prev){
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    // insertFirst
+
+    public void insertFirst(int data){
+        Node node = new Node(data);
+        node.next = head;
+        node.prev = null;
+        if(head != null){
+            head.prev = null;
+        }
+        head = node;
+    }
+    // Display The data
+
+    public void Display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "=>");
+            temp = temp.next;
+        }
+        System.out.println("End");
+    }
     
 }
--- DoublyLinkedList/DoublyMain.java
+++ DoublyLinkedList/DoublyMain.java
@@ -1,5 +1,10 @@
 package DoublyLinkedList;
 
 public class DoublyMain {
-    
+    public static void main(String[] args) {
+        DoublyLL ll = new DoublyLL();
+        ll.insertFirst(12);
+        ll.insertFirst(18);
+        ll.Display();
+    }
 }
