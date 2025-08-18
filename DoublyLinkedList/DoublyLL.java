4a8f7e480434caca89e2a5629d790649af40ea58
Anshuman <anshumanshukla.12as@gmail.com>
2025-08-18 23:38:30 +0530

    Comment Out the Constructor

--- DoublyLinkedList/DoublyLL.java
+++ DoublyLinkedList/DoublyLL.java
@@ -11,12 +11,12 @@ public class DoublyLL {
         public Node(int data){
             this.data = data;
         }
-        public Node(int data,Node next , Node prev){
-            this.data = data;
-            this.next = next;
-            this.prev = prev;
+        // public Node(int data,Node next , Node prev){
+        //     this.data = data;
+        //     this.next = next;
+        //     this.prev = prev;
             
-        }
+        // }
     }
 
     // insertFirst
