9e7eb72b6a299647fedca5410af3968e20d6e3e4
Anshuman <anshumanshukla.12as@gmail.com>
2025-08-19 12:51:10 +0530

    Singly LinkedList

--- SinglyLinkedList/SinglyLL.java
+++ SinglyLinkedList/SinglyLL.java
@@ -1,3 +1,72 @@
 public class SinglyLL {
-    
+    private Node head;
+    private Node tail;
+    private int size;
+    public SinglyLL(){
+        this.size = 0;
+    }
+    private class Node{
+        int data;
+        Node next;
+        
+        public Node(int data){
+            this.data = data;
+        }
+        public Node(int data,Node next){
+            this.data = data;
+            this.next = next;
+        }
+       
+    }
+    // insert First
+    public void insertFirst(int data){
+         Node node = new Node(data);
+        node.next = head;
+        head = node;
+
+        if (tail == null) {
+            tail = head;
+        } 
+        size++;  
+    }
+    // Insert Last
+    public void insertLast(int data) {
+        if (tail == null) {
+            insertFirst(data);
+            return;
+        }
+        Node node = new Node(data);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+    // Insert Index
+    public void insertWithIndex(int data, int index) {
+        if (index == 0) {
+            insertFirst(data);
+            return;
+        }
+        if (index == size) {
+            insertLast(data);
+            return;
+        }
+
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+
+        Node node = new Node(data, temp.next);
+        temp.next = node;
+
+        
+    }
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
 }
--- SinglyLinkedList/SinglyMain.java
+++ SinglyLinkedList/SinglyMain.java
@@ -1,5 +1,14 @@
 
 
 public class SinglyMain {
-    
+    public static void main(String[] args) {
+        SinglyLL ll = new SinglyLL();
+        ll.insertFirst(12);
+        ll.insertFirst(24);
+        ll.insertFirst(36);
+        ll.insertFirst(48);
+        ll.insertLast(51);
+        ll.insertWithIndex(101, 3);
+        ll.display();
+    }
 }
